71636cc28b92c1c89a00737876069847c45238fd
Mateus Sampaio Luz Silva <mateussampaio2710@gmail.com>
2025-10-10 23:43:22 -0300

    fix: remocao alert e correcoes

--- src/views/Panel/Panel.view.tsx
+++ src/views/Panel/Panel.view.tsx
@@ -1,11 +1,9 @@
-import { useContext } from "react"
 import Header from "../../components/Header/Header"
-import { Alert, AlertContext} from "@moreirapontocom/npmhelpers"
 import { Outlet } from "react-router-dom"
 import Footer from "../../components/Footer/Footer"
 
 const PanelView = () => {
-  const {alert} = useContext(AlertContext)
+  
   return <>
     <Header />
     <Outlet />
